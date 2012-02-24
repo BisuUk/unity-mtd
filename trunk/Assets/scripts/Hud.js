17f889d89477ceeb9b24c43765201797185c4c7e
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-02-24 02:37:17 +0000

    More HUD, handle it.

--- trunk/Assets/scripts/Hud.js
+++ trunk/Assets/scripts/Hud.js
@@ -4,7 +4,7 @@ import GUIControls;
 
 var colorCircle : Texture2D;
 var col : Color = Color.white;
-var selGridInt : int = 0;
+var selectedSides : int = 0;
 var selStrings : String[] = ["3", "4", "5", "6", "7", "8"];
 var vSliderValue : float = 0.0;// The variable to control where the scrollview 'looks' into its child elements.
 var scrollPosition : Vector2;
@@ -21,36 +21,34 @@ function OnGUI ()
 	GUILayout.EndArea();
 	
 	xOffset += 20;
-	selGridInt = GUI.SelectionGrid(Rect(xOffset, yOffset, 150, hudPanelHeight), selGridInt, selStrings, 2);
+	selectedSides = GUI.SelectionGrid(Rect(xOffset, yOffset, 150, hudPanelHeight), selectedSides, selStrings, 2);
 	
 	xOffset += 160;
 	vSliderValue = GUI.VerticalSlider(Rect(xOffset, yOffset+10, 30, hudPanelHeight-20), vSliderValue, 10.0, 0.0);
-	
-	xOffset += 40;
-    // Begin a scroll view. All rects are calculated automatically - 
-    // it will use up any available screen space and make sure contents flow correctly.
-    // This is kept small with the last two parameters to force scrollbars to appear.
-    
-	GUILayout.BeginArea(Rect(xOffset, yOffset, 300, hudPanelHeight));	    
-	    scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(300), GUILayout.Height(hudPanelHeight));
-	    
-		    // We just add a single label to go inside the scroll view. Note how the
-		    // scrollbars will work correctly with wordwrap.
-		    GUILayout.Label(longString);
-		    GUILayout.Button("GET FUCKED!",GUILayout.Width(50), GUILayout.Height(50));
-		    GUILayout.Label("GET FUCKED!");
-		    GUILayout.Label("GET FUCKED!");
-		    GUILayout.Label("GET FUCKED!");
-		    GUILayout.Label("GET FUCKED!");
-		    GUILayout.Label("GET FUCKED!");
-		    
-		    // Add a button to clear the string. This is inside the scroll area, so it
-		    // will be scrolled as well. Note how the button becomes narrower to make room
-		    // for the vertical scrollbar
-		    if (GUILayout.Button("Clear"))
-		        longString = "";
-	    
-	    // End the scrollview we began above.
+
+	xOffset += 200;
+	GUILayout.BeginArea(Rect(xOffset, yOffset+10, 50, hudPanelHeight));
+    	GUILayout.BeginVertical("box");
+		GUILayout.Button("New",GUILayout.Width(40), GUILayout.Height(40));
+	    GUILayout.Button("Del",GUILayout.Width(40), GUILayout.Height(40));
+	    GUILayout.Button("+",GUILayout.Width(40), GUILayout.Height(40));
+	    GUILayout.Button("-",GUILayout.Width(40), GUILayout.Height(40));
+	    GUILayout.EndVertical();		    		
+	GUILayout.EndArea();
+			
+	xOffset += 60;
+	GUILayout.BeginArea(Rect(xOffset, yOffset, 270, hudPanelHeight));	    
+	    scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(245), GUILayout.Height(hudPanelHeight));
+	    	var i : int = 0;
+	    	for (i=0; i<10; i++)
+	    	{
+		    	GUILayout.BeginHorizontal("box");
+				GUILayout.Button("SQ1"+i,GUILayout.Width(50), GUILayout.Height(50));
+			    GUILayout.Button("SQ2"+i,GUILayout.Width(50), GUILayout.Height(50));
+			    GUILayout.Button("SQ3"+i,GUILayout.Width(50), GUILayout.Height(50));
+			    GUILayout.Button("SQ4"+i,GUILayout.Width(50), GUILayout.Height(50));
+			    GUILayout.EndHorizontal();		    
+	    	}
 	    GUILayout.EndScrollView();	    
 	GUILayout.EndArea();
 }
