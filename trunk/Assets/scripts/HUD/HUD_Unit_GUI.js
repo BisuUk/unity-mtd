a8268583da297757cd0810ea23e85ea26a7fbe62
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-02-29 22:48:49 +0000

    Add squad prep.

--- trunk/Assets/scripts/HUD/HUD_Unit_GUI.js
+++ trunk/Assets/scripts/HUD/HUD_Unit_GUI.js
@@ -19,60 +19,73 @@ private var selStrings : String[] = ["3", "4", "5", "6", "7", "8"];
 
 function OnGUI ()
 {
-	var hudPanelHeight : int = 200;
-	var xOffset : int = hudPanelHeight;
-	var yOffset : int = Screen.height-hudPanelHeight;
-	
-	// Color wheel
-	GUILayout.BeginArea(Rect(0, yOffset, hudPanelHeight, hudPanelHeight));
-		selectedColor = RGBCircle(selectedColor, "", colorCircle);
-	GUILayout.EndArea();
-	
-	// Button Grid
-	xOffset += 20;
-	selectedSides = GUI.SelectionGrid(Rect(xOffset, yOffset, 150, hudPanelHeight), selectedSides, selStrings, 2);
-	
-	// Size slider
-	xOffset += 160;
-	selectedSize = GUI.VerticalSlider(Rect(xOffset, yOffset+10, 30, hudPanelHeight-20), selectedSize, 1.0, 0.0);
+   var hudPanelHeight : int = 200;
+   var xOffset : int = hudPanelHeight;
+   var yOffset : int = Screen.height-hudPanelHeight;
+   
+   // Color wheel
+   GUILayout.BeginArea(Rect(0, yOffset, hudPanelHeight, hudPanelHeight));
+   selectedColor = RGBCircle(selectedColor, "", colorCircle);
+   GUILayout.EndArea();
+   
+   // Button Grid
+   xOffset += 20;
+   selectedSides = GUI.SelectionGrid(Rect(xOffset, yOffset, 150, hudPanelHeight), selectedSides, selStrings, 2);
+   
+   // Size slider
+   xOffset += 160;
+   selectedSize = GUI.VerticalSlider(Rect(xOffset, yOffset+10, 30, hudPanelHeight-20), selectedSize, 1.0, 0.0);
+   
+   // Move 3D preview to be in correct location
+   xOffset += 20;
+   hudUnitPreviewCamera.camera.pixelRect = Rect(xOffset, 10, 180, hudPanelHeight-20);
+   
+   // Squad controls
+   xOffset += 190;
+   GUILayout.BeginArea(Rect(xOffset, yOffset+10, 50, hudPanelHeight));
+   GUILayout.BeginVertical("box");
+   if (GUILayout.Button("New",GUILayout.Width(40), GUILayout.Height(40)))
+   {
+      numSquads += 1;
+   }
+   if (GUILayout.Button("Del",GUILayout.Width(40), GUILayout.Height(40)))
+   {
+      numSquads -= 1;
+      if (numSquads < 0)
+         numSquads = 0;
+   }
+   GUILayout.Button("+",GUILayout.Width(40), GUILayout.Height(40));
+   GUILayout.Button("-",GUILayout.Width(40), GUILayout.Height(40));
+   GUILayout.EndVertical();
+   GUILayout.EndArea();
+   
+   // Squad inventory
+   xOffset += 60;
+   GUILayout.BeginArea(Rect(xOffset, yOffset, 270, hudPanelHeight));
+      scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(245), GUILayout.Height(hudPanelHeight));
 
-	// Move 3D preview to be in correct location
-	xOffset += 20;
-	hudUnitPreviewCamera.camera.pixelRect = Rect(xOffset, 10, 180, hudPanelHeight-20);
+         var invCols : int = 270/55;
+         var colCount : int = 0;
+   
+         GUILayout.BeginHorizontal("box");
 
-	// Squad controls
-	xOffset += 190;
-	GUILayout.BeginArea(Rect(xOffset, yOffset+10, 50, hudPanelHeight));
-    	GUILayout.BeginVertical("box");
-		if (GUILayout.Button("New",GUILayout.Width(40), GUILayout.Height(40)))
-		{
-			numSquads += 1;
-		}
-	    if (GUILayout.Button("Del",GUILayout.Width(40), GUILayout.Height(40)))
-	    {
-	    	numSquads -= 1;
-	    	if (numSquads < 0)
-	    		numSquads = 0;
-	    }
-	    GUILayout.Button("+",GUILayout.Width(40), GUILayout.Height(40));
-	    GUILayout.Button("-",GUILayout.Width(40), GUILayout.Height(40));
-	    GUILayout.EndVertical();		    		
-	GUILayout.EndArea();
-	
-	// Squad inventory
-	xOffset += 60;
-	GUILayout.BeginArea(Rect(xOffset, yOffset, 270, hudPanelHeight));	    
-	    scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(245), GUILayout.Height(hudPanelHeight));
-	    	var i : int = 0;
-	    	for (i=0; i<10; i++)
-	    	{
-		    	GUILayout.BeginHorizontal("box");
-				GUILayout.Button("SQ1"+i,GUILayout.Width(50), GUILayout.Height(50));
-			    GUILayout.Button("SQ2"+i,GUILayout.Width(50), GUILayout.Height(50));
-			    GUILayout.Button("SQ3"+i,GUILayout.Width(50), GUILayout.Height(50));
-			    GUILayout.Button("SQ4"+i,GUILayout.Width(50), GUILayout.Height(50));
-			    GUILayout.EndHorizontal();		    
-	    	}
-	    GUILayout.EndScrollView();	    
-	GUILayout.EndArea();
+         var i : int = 0;
+         for (i=0; i<numSquads; i++)
+         {
+            GUILayout.Button("SQ"+i,GUILayout.Width(50), GUILayout.Height(50));
+            //GUILayout.Toggle(false, "SQ"+i,GUILayout.Width(50), GUILayout.Height(50));
+   
+            colCount++;
+            if (colCount >= invCols)
+            {
+               colCount = 0;
+               GUILayout.EndHorizontal();
+               GUILayout.BeginHorizontal("box");
+            }
+         }
+         if (colCount < invCols)
+            GUILayout.EndHorizontal();
+
+      GUILayout.EndScrollView();       
+   GUILayout.EndArea();
 }
