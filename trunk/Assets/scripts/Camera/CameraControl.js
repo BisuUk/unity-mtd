35aa22ec15df467900456fa92e4c1a8c03046814
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-02-29 04:48:43 +0000

    Setting up emitter.

--- trunk/Assets/scripts/Camera/CameraControl.js
+++ trunk/Assets/scripts/Camera/CameraControl.js
@@ -5,76 +5,61 @@ var panSpeed : float;
 
 function Update ()
 {
-	
-	// Mouse click right
-	if (Input.GetMouseButton(1))
-	{
-		transform.Translate(Input.GetAxis("Mouse X")*-panSpeed, Input.GetAxis("Mouse Y")*-panSpeed, 0);
-	}
+   // Mouse click right (pan)
+   if (Input.GetMouseButton(1))
+      transform.Translate(Input.GetAxis("Mouse X")*-panSpeed, Input.GetAxis("Mouse Y")*-panSpeed, 0);
 
+   // Mouse wheel (zoom)
+   transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel")*zoomSpeed);
 
-	// Mouse wheel zoom
-	if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
-    {
-    Debug.Log("ypos ="+transform.position.y);
-    	if (transform.position.y >= 5)
-    	{    
-        	transform.Translate(0,0,-zoomSpeed);
-        }
-
-    }
-    if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
-    {
-
-        	transform.Translate(0,0,zoomSpeed);        
-        
-    }		
+   // Camera doesn't go below game board
+   if (transform.position.y <= 3)
+      transform.position.y = 3;
 }
 
 
 /*
 function Start ()
 {
-	// Make the rigid body not change rotation
-	if (rigidbody)
-		rigidbody.freezeRotation = true;
+ // Make the rigid body not change rotation
+ if (rigidbody)
+    rigidbody.freezeRotation = true;
 }
 
 
+public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+public RotationAxes axes = RotationAxes.MouseXAndY;
+public float sensitivityX = 15F;
+public float sensitivityY = 15F;
 
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
+public float minimumX = -360F;
+public float maximumX = 360F;
 
-	public float minimumX = -360F;
-	public float maximumX = 360F;
+public float minimumY = -60F;
+public float maximumY = 60F;
 
-	public float minimumY = -60F;
-	public float maximumY = 60F;
+float rotationY = 0F;
+ if (Input.GetMouseButton(1))
+ {
+    if (axes == RotationAxes.MouseXAndY)
+    {
+       float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
-	float rotationY = 0F;
-		if (Input.GetMouseButton(1))
-		{
-			if (axes == RotationAxes.MouseXAndY)
-			{
-				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-				
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-			}
-			else if (axes == RotationAxes.MouseX)
-			{
-				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-			}
-			else
-			{
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-			}
-		}
+       rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+       rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+       transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+    else if (axes == RotationAxes.MouseX)
+    {
+       transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+    }
+    else
+    {
+       rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+       rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+       transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+    }
+ }
 */
---
+++ trunk/Assets/scripts/Emitter/EmitterControl.js
@@ -0,0 +1,28 @@
+#pragma strict
+
+function Start () {
+
+}
+
+function Update ()
+{
+   // On rate interval
+      // Check queue contents
+      // Launch unit of squad type
+      // if top squad count = 0
+        // pop squad off queue
+}
+
+function OnMouseDown()
+{
+}
+
+function OnMouseEnter()
+{
+   renderer.material.color = Color.red;
+}
+
+function OnMouseExit()
+{
+   renderer.material.color = Color.white;
+}
