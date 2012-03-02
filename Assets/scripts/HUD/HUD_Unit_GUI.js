3f739c8cc0e1ce0c9127d974feab480df0caa299
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-03-02 05:15:58 +0000

    Unit HUD GUI about 90% complete.

--- Assets/scripts/HUD/HUD_Unit_GUI.js
+++ Assets/scripts/HUD/HUD_Unit_GUI.js
@@ -10,7 +10,7 @@ static var selectedSides : int = 0;
 // Editor
 var hudUnitPreviewCamera : GameObject;
 var colorCircle : Texture2D;
-var numSquads : int;
+var squadID : int;
 var invButtonStyle : GUIStyle;
 //var invButtonToggleTexture : Texture;
 var playerObject : GameObject;
@@ -18,27 +18,36 @@ var playerObject : GameObject;
 // This Script only
 private var scrollPosition : Vector2;
 private var selStrings : String[] = ["3", "4", "5", "6", "7", "8"];
+private var playerData : PlayerData;
+
+function Start()
+{
+   if (playerObject)
+      playerData = playerObject.GetComponent(PlayerData);
+}
 
 function OnGUI ()
 {
    var hudPanelHeight : int = 200;
    var xOffset : int = hudPanelHeight;
    var yOffset : int = Screen.height-hudPanelHeight;
-   var i : int = 0;
 
    // Color wheel
    GUILayout.BeginArea(Rect(0, yOffset, hudPanelHeight, hudPanelHeight));
    selectedColor = RGBCircle(selectedColor, "", colorCircle);
+   playerData.SetSquadColor(playerData.selectedSquadID,selectedColor);
    GUILayout.EndArea();
    
    // Button Grid
    xOffset += 20;
    selectedSides = GUI.SelectionGrid(Rect(xOffset, yOffset, 150, hudPanelHeight), selectedSides, selStrings, 2);
-   
+   playerData.SetSquadSides(playerData.selectedSquadID,selectedSides);
+
    // Size slider
    xOffset += 160;
    selectedSize = GUI.VerticalSlider(Rect(xOffset, yOffset+10, 30, hudPanelHeight-20), selectedSize, 1.0, 0.0);
-   
+   playerData.SetSquadSize(playerData.selectedSquadID,selectedSize);
+
    // Move 3D preview to be in correct location
    xOffset += 20;
    hudUnitPreviewCamera.camera.pixelRect = Rect(xOffset, 10, 180, hudPanelHeight-20);
@@ -49,16 +58,25 @@ function OnGUI ()
    GUILayout.BeginVertical("box");
    if (GUILayout.Button("New",GUILayout.Width(40), GUILayout.Height(40)))
    {
-      numSquads += 1;
+      var newSquad = new UnitSquad();
+      newSquad.id = squadID;// request from server.
+      squadID += 1;
+      newSquad.color = selectedColor;
+      playerData.AddSquad(newSquad);
+
    }
    if (GUILayout.Button("Del",GUILayout.Width(40), GUILayout.Height(40)))
    {
-      numSquads -= 1;
-      if (numSquads < 0)
-         numSquads = 0;
+      playerData.RemoveSquad(playerData.selectedSquadID);
    }
-   GUILayout.Button("+",GUILayout.Width(40), GUILayout.Height(40));
-   GUILayout.Button("-",GUILayout.Width(40), GUILayout.Height(40));
+   if (GUILayout.Button("+",GUILayout.Width(40), GUILayout.Height(40)))
+   {
+      playerData.IncrementSquad(playerData.selectedSquadID);
+   }
+   if (GUILayout.Button("-",GUILayout.Width(40), GUILayout.Height(40)))
+   {
+      playerData.DecrementSquad(playerData.selectedSquadID);
+   }
    GUILayout.EndVertical();
    GUILayout.EndArea();
 
@@ -73,11 +91,22 @@ function OnGUI ()
    
          GUILayout.BeginHorizontal("box");
 
-         for (i=0; i<numSquads; i++)
+         for (var sID in playerData.squads.Keys)
          {
-            if (GUILayout.Toggle(PlayerData.selectedSquad==i, "x"+i, invButtonStyle, GUILayout.Width(50), GUILayout.Height(50)))
+            var str : String;
+            var squad : UnitSquad = playerData.squads[sID];
+            var buttonSelected : boolean = playerData.selectedSquadID==sID;
+
+            if (squad.count > 1)
+               str = "x"+squad.count;
+
+            if (GUILayout.Toggle(buttonSelected, str, invButtonStyle, GUILayout.Width(50), GUILayout.Height(50)))
             {
-               PlayerData.selectedSquad = i;
+               // clicked this button
+               playerData.selectedSquadID = sID;
+               selectedColor = squad.color;
+               selectedSize = squad.size;
+               selectedSides = squad.sides;
             }
    
             colCount++;
--- Assets/scripts/Player/PlayerData.js
+++ Assets/scripts/Player/PlayerData.js
@@ -1,10 +1,61 @@
 #pragma strict
+import System.Collections.Generic;
 
 // Array of squads?
-static var selectedSquad : int;
+var selectedSquadID : int;
+var squads : Dictionary.<int,UnitSquad>;
 
 function Start () {
-   selectedSquad = -1;
+   selectedSquadID = -1;
+   squads = new Dictionary.<int,UnitSquad>();
+}
+
+function AddSquad(squad : UnitSquad)
+{
+   squad.count = 1;
+   squads[squad.id] = squad;
+   selectedSquadID = squad.id;
+}
+
+function RemoveSquad(squadID : int)
+{
+   squads.Remove(squadID);
+   selectedSquadID = -1;
+}
+
+function IncrementSquad(squadID : int)
+{
+   squads[squadID].count += 1;
+}
+
+function DecrementSquad(squadID : int)
+{
+   if (squads[squadID].count > 1)
+      squads[squadID].count -= 1;
+}
+
+function SetSquadColor(squadID : int, color : Color)
+{
+   if (squads.ContainsKey(squadID))
+   {
+      squads[squadID].color = color;
+   }
+}
+
+function SetSquadSides(squadID : int, sides : int)
+{
+   if (squads.ContainsKey(squadID))
+   {
+      squads[squadID].sides = sides;
+   }
+}
+
+function SetSquadSize(squadID : int, size : float)
+{
+   if (squads.ContainsKey(squadID))
+   {
+      squads[squadID].size = size;
+   }
 }
 
 function Update () {
