a47bee80ef8d5842a6180d9477f34ff693d5cdb9
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-03-02 05:15:58 +0000

    Unit HUD GUI about 90% complete.

---
+++ trunk/Assets/scripts/General/UnitTypes.js
@@ -0,0 +1,19 @@
+#pragma strict
+
+class UnitSquad
+{
+   var sides : int;
+   var color : Color;
+   var size  : float;
+   var count : int;
+   var id : int;
+   var deployed : boolean;
+};
+
+function Start () {
+
+}
+
+function Update () {
+
+}
