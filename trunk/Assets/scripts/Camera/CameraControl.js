78be2cd1c83cd4b240a01f18f059f98588bd0241
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-03-28 00:04:42 +0000

    Fixed camera reset lerp. Cleanup camera controls a little.

--- trunk/Assets/scripts/Camera/CameraControl.js
+++ trunk/Assets/scripts/Camera/CameraControl.js
@@ -9,21 +9,24 @@ var orbitOffset: float = 7.5;
 private var cameraAimPosition : Vector3;
 private var resetPosition : Vector3;
 private var resetOrientation : boolean = false;
+private var resetOrientStartTime : float;
+private var resetOrientDuration : float = 1.0;
+private var resetOrientLerp : float = 0.0;
 
-function Start()
-{
-}
+
 
 function Pan(amount : Vector3)
 {
    transform.Translate(amount);
 }
 
-function LateUpdate ()
+function LateUpdate()
 {
    var adjustPanSpeed : float = panSpeed * (transform.position.y * panZoomDamping);
+   var panAmount : Vector3 = Vector3.zero;
 
-   // MMB
+
+   // MMB & spacebar
    if (Input.GetMouseButton(2) || Input.GetKey (KeyCode.Space))
    {
       // If we were resetting view, user can override
@@ -39,7 +42,10 @@ function LateUpdate ()
          //Debug.Log("x="+transform.localRotation.eulerAngles.x);
       }
       else // pan camera
-         Pan(Vector3(Input.GetAxis("Mouse X")*-adjustPanSpeed, Input.GetAxis("Mouse Y")*-adjustPanSpeed, 0));
+      {
+         panAmount = Vector3(Input.GetAxis("Mouse X")*-adjustPanSpeed, Input.GetAxis("Mouse Y")*-adjustPanSpeed, 0);
+      }
+
    }
 
    // Mouse wheel (zoom)
@@ -50,18 +56,7 @@ function LateUpdate ()
       transform.Translate(0, 0, wheelDelta*zoomSpeed);
    }
 
-   if (resetOrientation)
-   {
-      //transform.localRotation.eulerAngles = Quaternion.Euler(90,0,0).eulerAngles;
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(90,0,0), Time.deltaTime*5.0);
-      transform.position = Vector3.Lerp(transform.position, resetPosition, Time.deltaTime*10.0);
-      // If we're rotated looking downward, and at 20 unit high, done resetting
-      if (transform.rotation == Quaternion.Euler(90,0,0) && Mathf.Approximately(transform.position.y, 20.0))
-         resetOrientation = false;
-   }
-
    // Arrow Keys
-   var panAmount : Vector3 = Vector3.zero;
    if (Input.GetKey (KeyCode.RightArrow))
       panAmount.x = adjustPanSpeed * Time.deltaTime * 10;
    else if (Input.GetKey (KeyCode.LeftArrow))
@@ -72,38 +67,36 @@ function LateUpdate ()
    else if (Input.GetKey (KeyCode.DownArrow))
       panAmount.y = -adjustPanSpeed * Time.deltaTime * 10;
 
+
+   if (edgeScreenScroll)
+   {
+      // Edge of screen scrolling
+      if (Input.mousePosition.x < 10)
+         panAmount.x = -adjustPanSpeed;
+      else if (Input.mousePosition.x > Screen.width-10)
+         panAmount.x = adjustPanSpeed;
+
+      if (Input.mousePosition.y < 10)
+         panAmount.y = -adjustPanSpeed;
+      else if (Input.mousePosition.y > Screen.height-10)
+         panAmount.y = adjustPanSpeed;
+   }
+
    if (panAmount != Vector3.zero)
    {
       resetOrientation = false;
       Pan(panAmount);
    }
 
-   if (edgeScreenScroll)
+   if (resetOrientation)
    {
-      // Edge of screen scrolling
-      if (Input.mousePosition.x < 10)
-      {
-         transform.Translate(-adjustPanSpeed, 0, 0);
+      resetOrientLerp = (Time.time-resetOrientStartTime)/resetOrientDuration;
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(90,0,0), resetOrientLerp);
+      transform.position = Vector3.Lerp(transform.position, resetPosition, resetOrientLerp);
+      // If we're rotated looking downward, and at 20 unit high, done resetting
+      if (transform.rotation == Quaternion.Euler(90,0,0) && Mathf.Approximately(transform.position.y, 20.0))
          resetOrientation = false;
-      }
-      else if (Input.mousePosition.x > Screen.width-10)
-      {
-         transform.Translate(adjustPanSpeed, 0, 0);
-         resetOrientation = false;
-      }
-      
-      if (Input.mousePosition.y < 10)
-      {
-         transform.Translate(0, -adjustPanSpeed, 0);
-         resetOrientation = false;
-      }
-      else if (Input.mousePosition.y > Screen.height-10)
-      {
-         transform.Translate(0, adjustPanSpeed, 0);
-         resetOrientation = false;
-      }
    }
-
 
    // Camera doesn't go below game board
    if (transform.position.y <= 3)
@@ -115,6 +108,7 @@ function Update ()
    // On backspace, initiate reset camera orientation
    if (Input.GetKeyDown(KeyCode.Backspace))
    {
+      resetOrientStartTime = Time.time;
       resetOrientation = true;
       resetPosition = transform.position;
       resetPosition.y = 20;
