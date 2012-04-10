2f6df0e1ee0848949288a1d0e4c6d3666cedf72a
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-04-10 19:03:18 +0000

    Renamed Tower to TowerUtil. Created a new Tower script that serves as a common baseclass for towers. Got Defend GUI in a good place. Starting to work in cost.

--- trunk/Assets/scripts/Tower/Tower.js
+++ trunk/Assets/scripts/Tower/Tower.js
@@ -1,95 +1,335 @@
 #pragma strict
+#pragma downcast
 
 static var baseScale : Vector3 = Vector3(0.50, 0.50, 0.50);
 static var baseRange : float = 2.5;
 static var baseFOV : float = 120.0;
 
-class InitData
-{
-   var range : float;
-   var fov   : float;
-   var color : Color;
-};
+var color : Color;
+var range : float = baseRange;
+var fov : float = baseFOV;
+var damage : int = 10;
+var buildTime : float = 1.0;
+var targetingBehavior : int = 1;
+var fireRate : float = 0.5;
+var isConstructing : boolean = false;
+var origRotation : Quaternion;
+var defaultMaterial: Material;
+var constructingMaterial : Material;
+var infoPlane : Transform;
+var netView : NetworkView;
+var AOEMeshFilter : MeshFilter;
+var AOEMeshRender: MeshRenderer;
+var AOEObject : Transform;
+private var constructionDuration : float;
+private var startConstructionTime : float = 0.0;
+private var endConstructionTime : float = 0.0;
+static private var playerData : PlayerData;
 
-static function PrefabName(type : int) : String
+private var kills : int = 0;    // Stats
+
+
+function Awake()
 {
-   var prefabName : String;
-   //switch (type)
-   //{
-   //   default:
-      prefabName = "prefabs/TowerPulsePrefab"; //break;
-   //}
-   return prefabName;
+   if (playerData == null)
+   {
+      var gameObj : GameObject = GameObject.Find("GameData");
+      playerData = gameObj.GetComponent(PlayerData);
+   }
+
+   AOEMeshRender.material = new Material(Shader.Find("Transparent/Diffuse"));
+}
+
+function Initialize(newFOV : float, newRange : float, newColor : Color)
+{
+   origRotation = transform.rotation;
+   AOEObject.parent = null; // Detach AOE mesh so that it doesn't rotate with the tower
+
+   SetFOV(newFOV);
+   SetRange(newRange); // make sure this is done after detach, or scaling will be wrong
+   SetColor(newColor);
+
+   // Init on server, and then send init info to clients
+   netView.RPC("Init", RPCMode.Others, newFOV, newRange, newColor.r, newColor.g, newColor.b);
+
+   // Start constructing visuals, and tell clients to do the same
+   SetConstructing(buildTime);
+   netView.RPC("SetConstructing", RPCMode.Others, buildTime);
+}
+
+@RPC
+function Init(newFOV : float, newRange : float, colorRed : float, colorGreen : float, colorBlue : float)
+{
+   origRotation = transform.rotation;
+   AOEObject.parent = null; // Detach AOE mesh so that it doesn't rotate with the tower
+
+   SetFOV(newFOV);
+   SetRange(newRange);
+   SetColor(Color(colorRed, colorGreen, colorBlue));
+}
+
+function Update()
+{
+   if (isConstructing)
+   {
+      // Animate model texture for that weird effect...
+      var offsetx : float = Time.time * 5.0;
+      var offsety : float = Time.time * 5.0;
+      renderer.material.SetTextureOffset("_MainTex", Vector2(offsetx,offsety));
+      for (var child : Transform in transform)
+      {
+         if (child != infoPlane && child != AOEObject)
+            child.renderer.material.SetTextureOffset("_MainTex", Vector2(offsetx,offsety));
+      }
+
+      // Animate clock gui texture
+      infoPlane.transform.position = transform.position + (Camera.main.transform.up*1.1);  //+ (Camera.main.transform.right*0.75);
+      var timerVal : float = (Time.time-startConstructionTime)/constructionDuration;
+      infoPlane.renderer.material.SetFloat("_Cutoff", Mathf.InverseLerp(0, 1, timerVal));
+
+      // Server checks completion time and informs clients
+      if (Network.isServer && Time.time >= endConstructionTime)
+      {
+         SetConstructing(0.0);
+         netView.RPC("SetConstructing", RPCMode.Others, 0.0);
+      }
+   }
+
+   // If this tower is selected, draw FOV
+   AOEMeshRender.enabled = (playerData.selectedTower == gameObject);
 }
 
 
-static function ScriptName(type : int) : String
+function SetRange(newRange : float)
 {
-   var scriptName : String;
-   //switch (type)
-   //{
-   //   default:
-      scriptName = "TowerPulse"; //break;
-   //}
-   return scriptName;
+   range = newRange;
+   if (range < Tower.baseRange)
+      range = Tower.baseRange;
+
+   AOEObject.transform.localScale = Vector3.one*range;
 }
 
 
-static function CreateAOEMesh(newAOE : float, scaleFactor : float) : Mesh
+function SetColor(newColor : Color)
 {
-   var x : int; //Counter
-   var stride : float = 10.0;
-
-   //Create a new mesh
-   var newAOEMesh : Mesh = new Mesh();
-
-   //Vertices
-   var vertex = new Vector3[newAOE/stride+3];
-
-   vertex[0] = Vector3.zero;
-   var r : Quaternion;
-   var i = 1;
-   for (x=-newAOE/2.0; x<=newAOE/2.0; x+=stride)
+   color = newColor;
+   renderer.material.color = color;
+   for (var child : Transform in transform)
    {
-      r = Quaternion.identity;
-      r *= Quaternion.Euler(0, x, 0);
-      vertex[i] = ((r*Vector3(0,0,1)*scaleFactor)); // one-over-localscale of parent.
-      i += 1;
+      if (child != infoPlane && child != AOEObject)
+         child.renderer.material.color = color;
    }
+   AOEMeshRender.material.color = color;
+   AOEMeshRender.material.color.a = 0.3;
+}
 
-   //UVs
-   var uvs = new Vector2[vertex.length];
-   for(x = 0; x < vertex.length; x++)
+
+function SetFOV(newFOV : float)
+{
+   fov = newFOV;
+   SetAOEMesh(newFOV);
+}
+
+private var lastAOE = -1;
+function SetAOEMesh(newAOE : float)
+{
+   if (lastAOE != newAOE)
    {
-      uvs[x] =  ((x%2) == 0) ? Vector2(0,0) : Vector2(1,1);
+      AOEMeshFilter.mesh = TowerUtil.CreateAOEMesh(newAOE, 1.0);
+      lastAOE = newAOE;
    }
+}
 
-   //Triangles
-   var tris = new int[3 * (vertex.length - 2)];    //3 verts per triangle * num triangles
-   var C1 : int = 0;
-   var C2 : int = 1;
-   var C3 : int = 2;
 
-   for(x = 0; x < tris.length; x+=3)
+@RPC
+function SetConstructing(duration : float)
+{
+   isConstructing = (duration > 0.0);
+
+   if (isConstructing)
    {
-      tris[x] = C1;
-      tris[x+1] = C2;
-      tris[x+2] = C3;
-      C2++;
-      C3++;
+      origRotation = transform.rotation; //set here for clients
+      constructionDuration = duration;
+      startConstructionTime = Time.time;
+      endConstructionTime = Time.time + constructionDuration;
+      infoPlane.renderer.enabled = true;
+
+      // Set model texture for that weird effect...
+      renderer.material = constructingMaterial;
+      renderer.material.SetColor("_TintColor", color);
+      for (var child : Transform in transform)
+      {
+         if (child != infoPlane && child != AOEObject)
+         {
+            child.renderer.material = constructingMaterial;
+            child.renderer.material.SetColor("_TintColor", color);
+         }
+      }
+
+      AOEMeshRender.material.color = color;
+      AOEMeshRender.material.color.a = 0.3;
    }
+   else
+   {
+      constructionDuration = 0.0;
+      startConstructionTime = 0.0;
+      endConstructionTime = 0.0;
+      infoPlane.renderer.enabled = false;
+      // Render normally - no build effect
+      renderer.material = defaultMaterial;
+      renderer.material.color = color;
+      for (var child : Transform in transform)
+      {
+         if (child != infoPlane && child != AOEObject)
+         {
+            child.renderer.material = defaultMaterial;
+            child.renderer.material.color = color;
+         }
+      }
+   }
+}
 
-   //Assign data to mesh
-   newAOEMesh.vertices = vertex;
-   newAOEMesh.uv = uvs;
-   newAOEMesh.triangles = tris;
 
-   //Recalculations
-   newAOEMesh.RecalculateNormals();
-   newAOEMesh.RecalculateBounds();
-   newAOEMesh.Optimize();
+// Find the the closest unit
+function FindClosestUnit() : GameObject
+{
+    // Find all game objects with tag Enemy
+    var gos : GameObject[] = GameObject.FindGameObjectsWithTag("UNIT");
+    var closest : GameObject; 
+    var distance = Mathf.Infinity; 
+    var position = transform.position; 
+    // Iterate through them and find the closest one
+    for (var go : GameObject in gos)
+    {
+        var diff = (go.transform.position - position);
+        var curDistance = diff.sqrMagnitude; 
+        if (curDistance < distance)
+        {
+            closest = go; 
+            distance = curDistance; 
+        }
+    }
+    return closest;    
+}
 
-   //Name the mesh
-   newAOEMesh.name = "FOVMesh";
-   return newAOEMesh;
+function FindTarget(checkLOS : boolean)
+{
+   var targ : GameObject = null;
+   var position = transform.position;
+   var closestDist = Mathf.Infinity;
+   var leastHealth = Mathf.Infinity;
+   var bestColorDiff = 0;
+
+   // Find all game objects with tag
+   var objs : GameObject[] = GameObject.FindGameObjectsWithTag("UNIT");
+
+   // Iterate through them and find the closest one
+   for (var obj : GameObject in objs)
+   {
+      var unitHealth : int = obj.GetComponent(Unit).health;
+      if (unitHealth > 0)
+      {
+         var diff = (obj.transform.position - position);
+         var dist = diff.magnitude;
+         // Check object is in range...
+         if (dist <= range)
+         {
+            // Check if object is in FOV...
+            var angle : float = Quaternion.Angle(Quaternion.LookRotation(diff), origRotation);
+            if (Mathf.Abs(angle) <= fov/2.0)
+            {
+               var pass : boolean = false;
+
+               if (checkLOS == false)
+                  pass = true;
+               else
+               {
+                  // Check if object is in line of sight
+                  var mask = (1 << 10); // BLOCKS
+                  if (Physics.Linecast(transform.position, obj.transform.position, mask)==false)
+                     pass = true;
+               }
+
+
+               if (pass)
+               {
+                  // Target closest
+                  if (targetingBehavior == 0)
+                  {
+                     if (dist < closestDist)
+                     {
+                        closestDist = dist;
+                        targ = obj;
+                     }
+                  }
+                  // Target weakest
+                  else if (targetingBehavior == 1)
+                  {
+                     if (unitHealth < leastHealth)
+                     {
+                        leastHealth = unitHealth;
+                        targ = obj;
+                     }
+                  }
+                  // Target best color
+                  else if (targetingBehavior == 2)
+                  {
+                     var unitColor : Color = obj.GetComponent(Unit).color;
+                     var rDmg : float = (1.0 - Mathf.Abs(color.r-unitColor.r));
+                     var gDmg : float = (1.0 - Mathf.Abs(color.g-unitColor.g));
+                     var bDmg : float = (1.0 - Mathf.Abs(color.b-unitColor.b));
+                     var colorDiff = rDmg + gDmg + bDmg;
+                     if (colorDiff > bestColorDiff)
+                     {
+                        bestColorDiff = colorDiff;
+                        targ = obj;
+                     }
+                  }
+               }
+            }
+         }
+      }
+   }
+   return targ;
+}
+
+function OnMouseDown()
+{
+   playerData.selectedTower = gameObject;
+}
+
+
+function SetDefaultBehaviorEnabled(setValue : boolean)
+{
+   enabled = setValue;
+}
+
+function OnDestroy()
+{
+   if (AOEObject)
+      Destroy(AOEObject);
+}
+
+function OnNetworkInstantiate(info : NetworkMessageInfo)
+{
+   // Network instantiated, turn on netview
+   netView.enabled = true;
+}
+
+
+function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo)
+{
+   //var pos : Vector3 = transform.position;
+   //stream.Serialize(pos);
+   var rot : Quaternion = transform.localRotation;
+   stream.Serialize(rot);
+
+   if (stream.isWriting)
+   {
+
+   }
+   else
+   {
+      transform.localRotation = rot;
+   }
 }
--- trunk/Assets/scripts/Tower/TowerPulse.js
+++ trunk/Assets/scripts/Tower/TowerPulse.js
@@ -1,116 +1,36 @@
 #pragma strict
 #pragma downcast
 
-var color : Color;
-var range : float = Tower.baseRange;
-var fov : float = Tower.baseFOV;
-var buildTime : float = 1.0;
-var baseDamage : int = 10;
-var targetingBehavior : int = 1;
-var fireRate : float = 0.5;
-var recoilDistance : float = 0.3;
-var recoilRecoverSpeed : float = 0.03;
-var defaultMaterial: Material;
-var constructingMaterial : Material;
-var lineRenderer : LineRenderer;
-var infoPlane : Transform;
 var barrelLeft : Transform;
 var barrelRight : Transform;
+var recoilDistance : float = 0.3;
+var recoilRecoverSpeed : float = 0.03;
 var laserPulsePrefab : Transform;
+var towerBase : Tower;
 var netView : NetworkView;
-var AOEMeshFilter : MeshFilter;
-var AOEMeshRender: MeshRenderer;
-var AOEObject : Transform;
-
-private var isConstructing : boolean = false;
-private var constructionDuration : float;
-private var startConstructionTime : float = 0.0;
-private var endConstructionTime : float = 0.0;
-private var origRotation : Quaternion;
 private var target : GameObject;
 private var laserPulse : Transform;
 private var nextFireTime : float;
 private var lastBarrelFired : Transform;
 private var origBarrelOffset : float;
-private var kills : int = 0;    // Stats
 static private var playerData : PlayerData;
-
 
 
 function Awake()
 {
    lastBarrelFired = barrelRight;
    origBarrelOffset = lastBarrelFired.localPosition.z;
-
-   if (playerData == null)
-   {
-      var gameObj : GameObject = GameObject.Find("GameData");
-      playerData = gameObj.GetComponent(PlayerData);
-   }
-
-   AOEMeshRender.material = new Material(Shader.Find("Transparent/Diffuse"));
-   //AOEObject.parent = null;
 }
 
-function Initialize(data : InitData)
-{
-   origRotation = transform.rotation;
-   AOEObject.parent = null; // Detach AOE mesh so that it doesn't rotate with the tower
-
-   // Init on server, and then send init info to clients
-   SetRange(data.range);
-   SetFOV(data.fov);
-   SetColor(data.color);
-   netView.RPC("Init", RPCMode.Others, data.fov, data.range, data.color.r, data.color.g, data.color.b);
-
-   // Start constructing visuals, and tell clients to do the same
-   SetConstructing(buildTime);
-   netView.RPC("SetConstructing", RPCMode.Others, buildTime);
-}
-
-@RPC
-function Init(newFov : float, newRange : float, colorRed : float, colorGreen : float, colorBlue : float)
-{
-   origRotation = transform.rotation;
-   AOEObject.parent = null; // Detach AOE mesh so that it doesn't rotate with the tower
-
-   SetFOV(fov);
-   SetRange(newRange);
-   SetColor(Color(colorRed, colorGreen, colorBlue));
-}
 
 function Update()
 {
-   if (isConstructing)
-   {
-      // Animate model texture for that weird effect...
-      var offsetx : float = Time.time * 5.0;
-      var offsety : float = Time.time * 5.0;
-      renderer.material.SetTextureOffset("_MainTex", Vector2(offsetx,offsety));
-      for (var child : Transform in transform)
-      {
-         if (child != infoPlane && child != AOEObject)
-            child.renderer.material.SetTextureOffset("_MainTex", Vector2(offsetx,offsety));
-      }
-
-      // Animate clock gui texture
-      infoPlane.transform.position = transform.position + (Camera.main.transform.up*1.1);  //+ (Camera.main.transform.right*0.75);
-      var timerVal : float = (Time.time-startConstructionTime)/constructionDuration;
-      infoPlane.renderer.material.SetFloat("_Cutoff", Mathf.InverseLerp(0, 1, timerVal));
-
-      // Server checks completion time and informs clients
-      if (Network.isServer && Time.time >= endConstructionTime)
-      {
-         SetConstructing(0.0);
-         netView.RPC("SetConstructing", RPCMode.Others, 0.0);
-      }
-   }
-   else // Not under construction, ready
+   if (towerBase.isConstructing==false)
    {
       // Server manages targeting behavior
       if (Network.isServer)
       {
-         var targ : GameObject = FindTarget();
+         var targ : GameObject = towerBase.FindTarget(false);
          if (targ)
          {
             transform.LookAt(targ.transform);
@@ -131,193 +51,8 @@ function Update()
       else if (lastBarrelFired.localPosition.z > origBarrelOffset)
          lastBarrelFired.localPosition.z = origBarrelOffset;
    }
-
-   // If this tower is selected, draw FOV
-   AOEMeshRender.enabled = (playerData.selectedTower == gameObject);
 }
 
-
-function SetRange(newRange : float)
-{
-   range = newRange;
-   if (range < Tower.baseRange)
-      range = Tower.baseRange;
-
-   AOEObject.transform.localScale = Vector3.one*range;
-}
-
-
-function SetColor(newColor : Color)
-{
-   color = newColor;
-   renderer.material.color = color;
-   for (var child : Transform in transform)
-   {
-      if (child != infoPlane && child != AOEObject)
-         child.renderer.material.color = color;
-   }
-   AOEMeshRender.material.color = color;
-   AOEMeshRender.material.color.a = 0.3;
-}
-
-
-function SetFOV(newFOV : float)
-{
-   fov = newFOV;
-   SetAOEMesh(newFOV);
-}
-
-private var lastAOE = -1;
-function SetAOEMesh(newAOE : float)
-{
-   if (lastAOE != newAOE)
-   {
-      AOEMeshFilter.mesh = Tower.CreateAOEMesh(newAOE, 1.0);
-      lastAOE = newAOE;
-   }
-}
-
-
-@RPC
-function SetConstructing(duration : float)
-{
-   isConstructing = (duration > 0.0);
-
-   if (isConstructing)
-   {
-      origRotation = transform.rotation; //set here for clients
-      constructionDuration = duration;
-      startConstructionTime = Time.time;
-      endConstructionTime = Time.time + constructionDuration;
-      infoPlane.renderer.enabled = true;
-
-      // Set model texture for that weird effect...
-      renderer.material = constructingMaterial;
-      renderer.material.SetColor("_TintColor", color);
-      for (var child : Transform in transform)
-      {
-         if (child != infoPlane && child != AOEObject)
-         {
-            child.renderer.material = constructingMaterial;
-            child.renderer.material.SetColor("_TintColor", color);
-         }
-      }
-
-      AOEMeshRender.material.color = color;
-      AOEMeshRender.material.color.a = 0.3;
-   }
-   else
-   {
-      constructionDuration = 0.0;
-      startConstructionTime = 0.0;
-      endConstructionTime = 0.0;
-      infoPlane.renderer.enabled = false;
-      // Render normally - no build effect
-      renderer.material = defaultMaterial;
-      renderer.material.color = color;
-      for (var child : Transform in transform)
-      {
-         if (child != infoPlane && child != AOEObject)
-         {
-            child.renderer.material = defaultMaterial;
-            child.renderer.material.color = color;
-         }
-      }
-   }
-}
-
-
-// Find the the closest unit
-function FindClosestUnit() : GameObject
-{
-    // Find all game objects with tag Enemy
-    var gos : GameObject[] = GameObject.FindGameObjectsWithTag("UNIT");
-    var closest : GameObject; 
-    var distance = Mathf.Infinity; 
-    var position = transform.position; 
-    // Iterate through them and find the closest one
-    for (var go : GameObject in gos)
-    {
-        var diff = (go.transform.position - position);
-        var curDistance = diff.sqrMagnitude; 
-        if (curDistance < distance)
-        {
-            closest = go; 
-            distance = curDistance; 
-        }
-    }
-    return closest;    
-}
-
-function FindTarget()
-{
-   var targ : GameObject = null;
-   var position = transform.position;
-   var closestDist = Mathf.Infinity;
-   var leastHealth = Mathf.Infinity;
-   var bestColorDiff = 0;
-
-   // Find all game objects with tag
-   var objs : GameObject[] = GameObject.FindGameObjectsWithTag("UNIT");
-
-   // Iterate through them and find the closest one
-   for (var obj : GameObject in objs)
-   {
-      var unitHealth : int = obj.GetComponent(Unit).health;
-      if (unitHealth > 0)
-      {
-         var diff = (obj.transform.position - position);
-         var dist = diff.magnitude;
-         // Check object is in range...
-         if (dist <= range)
-         {
-            // Check if object is in FOV...
-            var angle : float = Quaternion.Angle(Quaternion.LookRotation(diff), origRotation);
-            if (Mathf.Abs(angle) <= fov/2.0)
-            {
-               // Check if object is in line of sight
-               //var mask = (1 << 10); // BLOCKS
-               //if (Physics.Linecast(transform.position, obj.transform.position, mask)==false)
-               //{
-                  // Target closest
-                  if (targetingBehavior == 0)
-                  {
-                     if (dist < closestDist)
-                     {
-                        closestDist = dist;
-                        targ = obj;
-                     }
-                  }
-                  // Target weakest
-                  else if (targetingBehavior == 1)
-                  {
-                     if (unitHealth < leastHealth)
-                     {
-                        leastHealth = unitHealth;
-                        targ = obj;
-                     }
-                  }
-                  // Target best color
-                  else if (targetingBehavior == 2)
-                  {
-                     var unitColor : Color = obj.GetComponent(Unit).color;
-                     var rDmg : float = (1.0 - Mathf.Abs(color.r-unitColor.r));
-                     var gDmg : float = (1.0 - Mathf.Abs(color.g-unitColor.g));
-                     var bDmg : float = (1.0 - Mathf.Abs(color.b-unitColor.b));
-                     var colorDiff = rDmg + gDmg + bDmg;
-                     if (colorDiff > bestColorDiff)
-                     {
-                        bestColorDiff = colorDiff;
-                        targ = obj;
-                     }
-                  }
-               //}
-            }
-         }
-      }
-   }
-   return targ;
-}
 
 @RPC
 function Fire(targetLocation : Vector3)
@@ -328,70 +63,33 @@ function Fire(targetLocation : Vector3)
    var tpl : TowerPulseLaser = pulse.gameObject.GetComponent(TowerPulseLaser);
    tpl.muzzlePosition = lastBarrelFired.transform.position;
    tpl.targetPosition = targetLocation;
-   tpl.laserColor = color;
+   tpl.laserColor = towerBase.color;
 
    // Recoil barrel
    lastBarrelFired.localPosition.z -= recoilDistance;
 
    // Set next time to fire
-   nextFireTime  = Time.time + fireRate;
+   nextFireTime  = Time.time + towerBase.fireRate;
 
    // Owner will apply damage to unit
    if (Network.isServer)
    {
       var tUnit : Unit = target.GetComponent(Unit);
-      var rDmg : float = (0.3333 * (1.0 - Mathf.Abs(color.r-tUnit.color.r)));
-      var gDmg : float = (0.3333 * (1.0 - Mathf.Abs(color.g-tUnit.color.g)));
-      var bDmg : float = (0.3333 * (1.0 - Mathf.Abs(color.b-tUnit.color.b)));
+      var rDmg : float = (0.3333 * (1.0 - Mathf.Abs(towerBase.color.r-tUnit.color.r)));
+      var gDmg : float = (0.3333 * (1.0 - Mathf.Abs(towerBase.color.g-tUnit.color.g)));
+      var bDmg : float = (0.3333 * (1.0 - Mathf.Abs(towerBase.color.b-tUnit.color.b)));
       //Debug.Log("TowerPulse:Fire: rDmg="+rDmg+" gDmg="+gDmg+" bDmg="+bDmg);
-      var dmg : int = baseDamage * (rDmg + gDmg + bDmg);
+      var dmg : int = towerBase.damage * (rDmg + gDmg + bDmg);
    
       //if (tUnit.DoDamage(dmg, color) == false)
-      tUnit.DoDamage(dmg, color.r, color.g, color.b);
+      tUnit.DoDamage(dmg, towerBase.color.r, towerBase.color.g, towerBase.color.b);
       //kills += 1;
    }
 }
 
-
-function OnMouseDown()
-{
-   playerData.selectedTower = gameObject;
-}
-
-
 function SetDefaultBehaviorEnabled(setValue : boolean)
 {
    enabled = setValue;
-}
-
-function OnDestroy()
-{
-   if (AOEObject)
-      Destroy(AOEObject);
-}
-
-function OnNetworkInstantiate(info : NetworkMessageInfo)
-{
-   // Network instantiated, turn on netview
-   netView.enabled = true;
-}
-
-
-function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo)
-{
-   //var pos : Vector3 = transform.position;
-   //stream.Serialize(pos);
-   var rot : Quaternion = transform.localRotation;
-   stream.Serialize(rot);
-
-   if (stream.isWriting)
-   {
-
-   }
-   else
-   {
-      transform.localRotation = rot;
-   }
 }
 
 
