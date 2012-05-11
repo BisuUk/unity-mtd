1d4b372c9baf858ae26b6520904bfb8f8f3f573f
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-05-11 20:42:38 +0000

    Fixed multiplayer to work with menu and level transition.

--- trunk/Assets/scripts/Game/MapData.js
+++ trunk/Assets/scripts/Game/MapData.js
@@ -6,32 +6,3 @@ var attackCreditInfusionFreq: float;
 var defendStartCredits : int;
 var defendCreditInfusionSize: int;
 var defendCreditInfusionFreq: float;
-var netView : NetworkView;
-
-private var nextAttackInfusionTime : float;
-private var nextDefendInfusionTime : float;
-
-@RPC
-function CreditInfusion(isAttacker : boolean, infusion : int)
-{
-   if (Game.player.isAttacker == isAttacker)
-      Game.player.credits += infusion;
-}
-
-function Update()
-{
-   if (Network.isServer)
-   {
-      if(Time.time >= nextAttackInfusionTime)
-      {
-         netView.RPC("CreditInfusion", RPCMode.All, true, attackCreditInfusionSize);
-         nextAttackInfusionTime = Time.time + attackCreditInfusionFreq;
-      }
-
-      if(Time.time >= nextDefendInfusionTime)
-      {
-         netView.RPC("CreditInfusion", RPCMode.All, false, defendCreditInfusionSize);
-         nextDefendInfusionTime = Time.time + defendCreditInfusionFreq;
-      }
-   }
-}
