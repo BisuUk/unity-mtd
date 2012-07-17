1fea5b0873dfe8db339f6525868876de025d448d
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-07-17 22:19:48 +0000

    Attempting to get towers working in 3D.

--- trunk/Assets/scripts/Tower/TowerUtil.js
+++ trunk/Assets/scripts/Tower/TowerUtil.js
@@ -22,14 +22,16 @@ static function CreateAOEMesh(newAOE : float, scaleFactor : float) : Mesh
 {
    var x : int; //Counter
    var stride : float = 10.0;
+   var height : float = 1.0;
 
    //Create a new mesh
    var newAOEMesh : Mesh = new Mesh();
 
    //Vertices
-   var vertex = new Vector3[newAOE/stride+3];
+   var vertex = new Vector3[(newAOE/stride+3)*2];
 
    vertex[0] = Vector3.zero;
+   vertex[1] = Vector3.zero + Vector3(0, height, 0);
    var r : Quaternion;
    var i = 1;
    for (x=-newAOE/2.0; x<=newAOE/2.0; x+=stride)
@@ -37,6 +39,8 @@ static function CreateAOEMesh(newAOE : float, scaleFactor : float) : Mesh
       r = Quaternion.identity;
       r *= Quaternion.Euler(0, x, 0);
       vertex[i] = ((r*Vector3(0,0,1)*scaleFactor)); // one-over-localscale of parent.
+      i += 1;
+      vertex[i] = vertex[i-1] + Vector3(0, height, 0);
       i += 1;
    }
 
@@ -53,13 +57,17 @@ static function CreateAOEMesh(newAOE : float, scaleFactor : float) : Mesh
    var C2 : int = 1;
    var C3 : int = 2;
 
+   var b : boolean = false;
    for(x = 0; x < tris.length; x+=3)
    {
       tris[x] = C1;
-      tris[x+1] = C2;
-      tris[x+2] = C3;
+
+      tris[x+1] = b ? C3 : C2;
+      tris[x+2] = b ? C2 : C3;
+      C1++;
       C2++;
       C3++;
+      b = !b;
    }
 
    //Assign data to mesh
