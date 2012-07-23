9cb49e7e509a175ceb5fe232e3506613f3af81b2
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-07-23 01:17:33 +0000

    Added in doubleclick snap, and improved camera controls.

--- Assets/scripts/Game/MapData.js
+++ Assets/scripts/Game/MapData.js
@@ -7,9 +7,13 @@ var attackCreditInfusionEndSize : int;
 var attackCreditInfusionFreq : float;
 var attackManaRechargeSize : int;
 var attackManaRechargeFreq : float;
+var attackDefaultCameraPos : Transform;
 var defendStartCredits : int;
 var defendCreditInfusionStartSize : int;
 var defendCreditInfusionEndSize : int;
 var defendCreditInfusionFreq : float;
 var defendManaRechargeSize : int;
 var defendManaRechargeFreq : float;
+var defendDefaultCameraPos : Transform;
+var topDownCameraPos : Transform;
+
