9c286be743bf3e76215fcae60a27634b180f85fd
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-08-10 20:28:05 +0000

    Made generic some fixed some shot FX scripts, so they can be used on models.

--- trunk/Assets/scripts/FX/LaserBeamFX.js
+++ trunk/Assets/scripts/FX/LaserBeamFX.js
@@ -1,7 +1,7 @@
 #pragma strict
 
-var muzzlePosition : Vector3;
-var targetPosition : Vector3;
+var startPosition : Transform;
+var endPosition : Vector3;
 var duration : float;
 var laserWidthLimit : Vector2;
 var laserColor : Color;
@@ -12,18 +12,18 @@ private var endTime : float;
 private var laserWidth : float = 0.0;
 private var laserPulsateUp : boolean = true;
 
-function Start ()
+function Start()
 {
    startTime = Time.time;
    endTime = Time.time + duration;
    LR = GetComponent(LineRenderer);
-   LR.SetPosition(0, muzzlePosition);
-   LR.SetPosition(1, targetPosition);
+   LR.SetPosition(0, startPosition.position);
+   LR.SetPosition(1, endPosition);
    LR.SetColors(laserColor, laserColor);
    laserWidth = laserWidthLimit.x;
 }
 
-function Update ()
+function Update()
 {
    // Shrink laser from limitmax to limitmin over duration time
    laserWidth = Mathf.Lerp( laserWidthLimit.x, laserWidthLimit.y, Mathf.InverseLerp(startTime, endTime, Time.time) );
--- trunk/Assets/scripts/FX/LightningBoltFX.js
+++ trunk/Assets/scripts/FX/LightningBoltFX.js
@@ -1,15 +1,16 @@
 #pragma strict
 
-var muzzlePosition : Vector3;
-var targetPosition : Vector3;
+var startPosition : Transform;
+var endPosition : Transform;
 var duration : float;
 var color : Color;
 var LR : LineRenderer;
-var laserWidth : float;
+var laserWidth : float = 1.0;
 // LIGHTNING BOLT EFFECT
 var arcLength = 2.0;
 var arcVariation = 2.0;
 var inaccuracy = 1.0;
+var maxVerts : int = 100;
 
 private var endTime : float;
 
@@ -22,25 +23,34 @@ function Start()
 
 function Update()
 {
-   var lastPoint = muzzlePosition;
+   //if (arcLength <= 0.0 || arcLength > 10.0)
+   //   return;
+   //if (arcVariation <= 0.0 || arcVariation > 10.0)
+   //   return;
+   if (arcLength <= 0.0 || arcVariation <= 0.0)
+      return;
+
+   var lastPoint = startPosition.position;
    var i = 1;
-   LR.SetPosition(0, transform.position);//make the origin of the LR the same as the transform
-   while (Vector3.Distance(targetPosition, lastPoint) >.5)
+   LR.SetPosition(0, startPosition.position);//make the origin of the LR the same as the transform
+   while (Vector3.Distance(endPosition.position, lastPoint) >.5)
    {
       //was the last arc not touching the target?
       LR.SetVertexCount(i + 1);//then we need a new vertex in our line renderer
-      var fwd = targetPosition - lastPoint;//gives the direction to our target from the end of the last arc
+      var fwd = endPosition.position - lastPoint;//gives the direction to our target from the end of the last arc
       fwd.Normalize();//makes the direction to scale
       fwd = Randomize(fwd, inaccuracy);//we don't want a straight line to the target though
       fwd *= Random.Range(arcLength * arcVariation, arcLength);//nature is never too uniform
       fwd += lastPoint;//point + distance * direction = new point. this is where our new arc ends
       LR.SetPosition(i, fwd);//this tells the line renderer where to draw to
       i++;
+      if (i > maxVerts)
+         break;
       lastPoint = fwd;//so we know where we are starting from for the next arc
       LR.SetWidth(laserWidth, laserWidth/2);
    }
 
-   if( Time.time > endTime )
+   if(duration > 0.0 && Time.time > endTime )
       Destroy(gameObject);
 }
 
