0e35178f91dd010ce3be9fd87ab10ef1eb7760a0
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-08-12 02:30:25 +0000

    Lightning experiment. Sounds so sciency.

--- Assets/scripts/FX/LightningBoltFX.js
+++ Assets/scripts/FX/LightningBoltFX.js
@@ -4,7 +4,8 @@ var LR : LineRenderer;
 var startPosition : Transform;
 var endPosition : Transform;
 var intervalLimits : Vector2;
-var intervalDuration : Vector2;
+var intervalDuration : float;
+var fadeOutSpeed : float;
 var duration : float;
 var color : Color;
 // LIGHTNING BOLT EFFECT
@@ -14,12 +15,22 @@ var arcVariation = 2.0;
 var inaccuracy = 1.0;
 var maxVerts : int = 100;
 
+
+private var fadeOutAlpha : float;
+
+
+
 function Start()
 {
+   fadeOutAlpha = 1.0;
    if (LR)
-      Invoke("GenerateBolt", Random.Range(intervalLimits.x, intervalLimits.y));
+      GenerateBolt();
    else
+   {
       Debug.Log("LightModFX=ERRORL No line renderer object!");
+      Destroy(gameObject, 0.5);
+      return;
+   }
 
    if (duration > 0.0)
       Invoke("OnDuration", duration);
@@ -27,12 +38,28 @@ function Start()
 
 function Update()
 {
-   LR.SetColors(color, color);
+   if (fadeOutSpeed > 0.0)
+   {
+      fadeOutAlpha -= fadeOutSpeed;
+      var c : Color = color;
+      c.a = fadeOutAlpha;
+      LR.SetColors(c, c);
+   }
+   else
+   {
+      LR.SetColors(color, color);
+   }
+
 }
 
 function OnDuration()
 {
    Destroy(gameObject);
+}
+
+function OnIntervalDuration()
+{
+   LR.SetVertexCount(0);
 }
 
 function GenerateBolt()
@@ -58,7 +85,11 @@ function GenerateBolt()
          lastPoint = fwd;//so we know where we are starting from for the next arc
          LR.SetWidth(lineWidth, lineWidth/2);
       }
+      fadeOutAlpha = 1.0;
    }
+   if (intervalDuration > 0.0)
+      Invoke("OnIntervalDuration", intervalDuration);
+
    Invoke("GenerateBolt", Random.Range(intervalLimits.x, intervalLimits.y));
 }
 
