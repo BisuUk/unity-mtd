50cd178d730b7944bafde0319d8e63466fc40142
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-08-25 22:44:52 +0000

    More Defend UI.

--- trunk/Assets/scripts/GUI/DefendUI.js
+++ trunk/Assets/scripts/GUI/DefendUI.js
@@ -1,5 +1,13 @@
 #pragma strict
 
+var controlAreaSets : Transform[];
+var colorArea : Transform;
+
+function Start()
+{
+   SwitchControlSet(0);
+   Utility.SetActiveRecursive(colorArea, false);
+}
 
 function CreateNewCursor(type : int)
 {
@@ -9,11 +17,16 @@ function CreateNewCursor(type : int)
 function OnMortarTower()
 {
    CreateNewCursor(3);
+   SwitchControlSet(1);
+   Utility.SetActiveRecursive(colorArea, true);
 }
 
 function OnRangedTower()
 {
    CreateNewCursor(1);
+   SwitchControlSet(1);
+   Utility.SetActiveRecursive(colorArea, true);
+   //var c : DefendGUICursor = GUIControl.cursorObject.GetComponent(DefendGUICursor);   
 }
 
 function OnSlowTower()
@@ -24,4 +37,14 @@ function OnSlowTower()
 function OnPainterTower()
 {
    CreateNewCursor(2);
+   SwitchControlSet(1);
+   colorArea.gameObject.active = true;
+}
+
+function SwitchControlSet(newSet : int)
+{
+   for (var i : int=0; i<controlAreaSets.length; i++)
+   {
+      Utility.SetActiveRecursive(controlAreaSets[i], (i == newSet));
+   }
 }
