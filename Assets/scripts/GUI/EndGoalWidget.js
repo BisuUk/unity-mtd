d0eb5151a0212dfac594eeb119057284fd500ebc
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-11-23 21:36:39 +0000

    End goal UI part 2. Now need icon graphics.

--- Assets/scripts/GUI/EndGoalWidget.js
+++ Assets/scripts/GUI/EndGoalWidget.js
@@ -6,17 +6,30 @@ var iconSpacing : float;
 
 var goal : GoalStation;
 
-private var icons : List.<Transform>;
+private var icons : List.<UISprite>;
+private var iconCount : int;
+
+function Awake()
+{
+   icons = new List.<UISprite>();
+}
 
 function AddUnitIcon(color : Color)
 {
-   var newWidget : GameObject = NGUITools.AddChild(iconStart.gameObject, iconPrefab.gameObject);
-   //GameObject.Instantiate(iconPrefab, Vector3.zero, Quaternion.identity);
+   var iconWidget : GameObject = NGUITools.AddChild(iconStart.gameObject, iconPrefab.gameObject);
+   var icon : UISprite = iconWidget.GetComponent(UISprite);
+   icon.color = color;
+   iconWidget.transform.localPosition.x += (iconSpacing * iconCount);
+   iconWidget.transform.localScale = Vector3(25.0, 25.0, 1.0);
+   icons.Add(icon);
+   iconCount += 1;
 }
 
 function UnitReachedGoal()
 {
    Debug.Log("UnitReachedGoal: index="+goal.lastFilledIndex);
+   icons[goal.lastFilledIndex].spriteName = "Light";
+
 }
 
 
