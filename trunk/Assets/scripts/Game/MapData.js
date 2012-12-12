c35262fee8195861321472c7b3f9eed23187aefa
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2012-12-12 22:13:42 +0000

    Changed map boundary code again to support rectangles instead of circles. Updated all levels to reflect new boundary mechanism. Switches now have a fire once flag.

--- trunk/Assets/scripts/Game/MapData.js
+++ trunk/Assets/scripts/Game/MapData.js
@@ -1,15 +1,11 @@
 #pragma strict
 
-
-var center : Transform;
-var boundaryRadius : float;
-var boundaryHeights : Vector2;
-
-var useCreditInfusions : boolean;
-var useCreditCapacities : boolean;
+var boundaryLower : Transform;
+var boundaryUpper : Transform;
 var topDownCameraPos : Transform;
 var nextLevel : String;
 
+// Puzzle mode
 var unitMax : int;
 var unitPar : int;
 var timeBonusLimit : float;
@@ -18,13 +14,15 @@ var underParBonusPerUnit : int;
 var noDeathBonus : int;
 var noAbilityBonus : int;
 
+// TD mode
+var useCreditInfusions : boolean;
+var useCreditCapacities : boolean;
 var attackStartCredits : int;
 var attackStartCreditCapacity : int;
 var attackCreditInfusionStartSize : int;
 var attackCreditInfusionEndSize : int;
 var attackCreditInfusionFreq : float;
 var attackDefaultCameraPos : Transform;
-
 var defendStartCredits : int;
 var defendCreditInfusionStartSize : int;
 var defendCreditInfusionEndSize : int;
