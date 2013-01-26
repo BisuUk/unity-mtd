315800f9abaefbc525cf541e0092e64555bc469d
devin.kelly <devin.kelly@5a3d667a-9c87-3ec6-ba46-12b4c284b1e0>
2013-01-26 06:17:24 +0000

    Fixed goal counting dudes twice.

--- Assets/scripts/Structures/GoalStation.js
+++ Assets/scripts/Structures/GoalStation.js
@@ -51,7 +51,7 @@ function OnTriggerStay(other : Collider)
          {
             isFull = CheckFull();
             Game.control.UnitReachedGoal(this);
-            Destroy(unit.gameObject, 0.05);
+            Destroy(unit.gameObject);
          }
          else
          {
